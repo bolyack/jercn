eafbc010fb488cce9e53abb84b519139a9c8faf6
bolyack <xdj927@126.com>
2017-07-14 16:36:26 +0800

    QR code

--- from-utils/src/main/java/com/bamboo/utils/io/DownLoadUtil.java
+++ from-utils/src/main/java/com/bamboo/utils/io/DownLoadUtil.java
@@ -3,12 +3,10 @@ package com.bamboo.utils.io;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * download file
@@ -57,5 +55,38 @@ public class DownLoadUtil {
         return bos;
     }
 
+    /* Spring mvc：online view image by ByteArrayOutputStream
+    @RequestMapping("common/downLoad.html")
+    public void fileLoad(HttpServletRequest request, HttpServletResponse response) {
+        String fileName = "二维码.jpg";
+        try{
+            if (request.getHeader("User-Agent").toUpperCase().indexOf("MSIE") > 0) {
+                fileName = URLEncoder.encode(fileName, "UTF-8");
+            } else {
+                fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        response.setCharacterEncoding("utf-8");
+        response.setHeader("Content-Type","image/jpeg");
+        response.setHeader("Content-disposition", "fileName=" + fileName);
+
+        OutputStream os = null;
+        try {
+            os = response.getOutputStream();
+            ByteArrayOutputStream bos = QRCodeUtil.encodeOutputStream("http://wwww.baidu.com?companyId=1&token=11123214242sdsa1", true);
+            os.write(bos.toByteArray());
+            os.flush();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            try {
+                if (null != os) os.close();
+            } catch (Exception e1) {
+                LOGGER.error(e1.getMessage(), e1);
+            }
+        }
+    }*/
 
 }
