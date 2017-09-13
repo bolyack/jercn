bd7643e1e7eb0f2c24c519963f1ee59defa9320c
bolyack <xdj927@126.com>
2017-09-13 13:28:27 +0800

    data util modify

--- from-utils/src/main/java/com/bamboo/utils/date/DateUsageUtils.java
+++ from-utils/src/main/java/com/bamboo/utils/date/DateUsageUtils.java
@@ -3,6 +3,8 @@ package com.bamboo.utils.date;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -28,7 +30,61 @@ public class DateUsageUtils {
 
 
     public static void main(String[] args) {
-        System.out.println(DateUsageUtils.getDateFormat().toString());
+//        System.out.println(DateUsageUtils.getDateFormat().toString());
+        ArrayList<String> list = getDayForPreOrNext(10, false);
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+
+
+    /**
+     * 获取过去或者未来 任意天内的日期数组
+     * @param intervals intervals天内
+     * @param pastFlag true-获取过去; false-获取将来
+     * @return 日期数组
+     */
+    public static ArrayList<String> getDayForPreOrNext(int intervals, boolean pastFlag) {
+        ArrayList<String> daysList = new ArrayList<String>();
+        if (pastFlag) {
+            for (int i = 0; i <intervals; i++) {
+                daysList.add(getPastDate(i));
+            }
+        } else {
+            for (int i = 0; i <intervals; i++) {
+                daysList.add(getFetureDate(i));
+            }
+        }
+        return daysList;
+    }
+
+    /**
+     * 获取过去第几天的日期
+     *
+     * @param past
+     * @return
+     */
+    public static String getPastDate(int past) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - past);
+        Date today = calendar.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String result = format.format(today);
+        return result;
+    }
+
+    /**
+     * 获取未来 第 past 天的日期
+     * @param past
+     * @return
+     */
+    public static String getFetureDate(int past) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) + past);
+        Date today = calendar.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String result = format.format(today);
+        return result;
     }
 
 }
